18a2b3375e10d4c28f6039a2536a0c9611d9593f
Preston Copeland <copelandps1@gmail.com>
2015-08-17 13:07:40 -0400

    first solution to Project Euler problem 17

--- p17_number_letter_counts.rb
+++ p17_number_letter_counts.rb
@@ -11,18 +11,26 @@ If all the numbers from 1 to 1000 (one thousand) inclusive were written out in w
 
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
+Answer: 21124 (which I got)
+
 =end
 
 def number_to_letter_total(start_range, end_range)
 
-  word_array = (start_range..end_range).map {|number| number_to_word(number) }
-  word_array.map {|word| letter_count(word) }.inject(:+)
+  (start_range..end_range).map {|number| letter_count(number_to_word(number))}.reduce(:+)
+
+end
+
+def letter_count(word)
+
+  word.split(" ").map {|word_part| word_part.length}.inject(:+)
 
 end
 
 def number_to_word(number)
+
   ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-  teens = %{ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
+  teens = ["ten", "eleven", "twelve", "thirteen", "fourteen",  "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
   tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
   return "one thousand" if (number == 1000)
@@ -31,10 +39,13 @@ def number_to_word(number)
   place = 10**2
   place_value = number/place
   remainder = number%place
-  word += " #{ones[place_value]} hundred"
+  word += " #{ones[place_value]} hundred" if(place_value != 0)
+  word += " and" if (remainder !=0 && place_value != 0)
+
   place = 10**1
   place_value = remainder/place
   remainder = remainder%place
+
   if(place_value == 0)
     return word += " #{ones[remainder]}"
   elsif(place_value == 1)
@@ -42,8 +53,10 @@ def number_to_word(number)
   else
     word += " #{tens[place_value]}"
   end
+
   word += " #{ones[remainder]}"
 
   word
 
 end
+
