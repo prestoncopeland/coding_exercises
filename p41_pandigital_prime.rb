a6a0ece90dc6384b87b3ac468746fa483d657d70
Preston Copeland <copelandps1@gmail.com>
2015-09-02 10:25:57 -0400

    final p41 pandigital prime optimization

--- p41_pandigital_prime.rb
+++ p41_pandigital_prime.rb
@@ -10,7 +10,7 @@ We shall say that an n-digit number is pandigital if it makes use of all the dig
 What is the largest n-digit pandigital prime that exists?
 
 Answer: 7652413 (solved myself)
-
+----------
 First version:
 Real runtime after rehearsal: 34 seconds
 
@@ -37,16 +37,16 @@ def largest_pandigital_prime
     end
   end
 end
-
-
-Second, optimized version: Using optimization that 8-digit and 9-digit numbers can't be done using elementary division theory (adding all 9 digits gives 45, which means a 9-digit number would be divisible by 3; similar reasoning gives 36 and 3-divisibility for all 8-digit numbers fitting the criteria). So, we start with 7-digit numbers. I gleaned this insight from the solution board for problem 41.
+----------
+Second, much better version: Using optimization that 8-digit and
+9-digit numbers can't be done using elementary division
+theory (adding all 9 digits gives 45, which means a
+9-digit number would be divisible by 3; similar reasoning
+gives 36 and 3-divisibility for all 8-digit numbers
+fitting the criteria). So, we start with 7-digit numbers.
+I gleaned this insight from the solution board for problem 41.
 
 Real runtime after rehearsal: 0.18 seconds (!!!!!!!)
-
-=end
-
-require 'prime'
-require 'benchmark'
 
 def largest_pandigital_prime
   pandigital = [1,2,3,4,5,6,7]
@@ -72,11 +72,35 @@ def largest_pandigital_prime
   end
 end
 
+
+----------
+Third, final version. Realized that I could avoid the need for a flag to check for pandigitality if I initialized the pandigital array more efficiently to start. This halved the LOC.
+Real runtime after rehearsal: 0.16 seconds
+
+=end
+
+require 'prime'
+require 'benchmark'
+
+def largest_pandigital_prime
+  7.downto(2) do |i|
+    pandigital = []
+    (1..i).each do |digit|
+      pandigital << digit
+    end
+    p = pandigital.permutation(i).to_a
+    p = p.map {|arr| arr.join.to_i}
+    p = p.keep_if {|v| Prime.prime?(v)}.sort
+    return p.max if(p != [])
+  end
+end
+
 Benchmark.bmbm do |benchmark|
   benchmark.report('largest pandigital prime') do
     largest_pandigital_prime
   end
 end
+
 
 
 
